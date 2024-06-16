a97baeef87880cc5f99e0760c1c57964727a8530
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-16 05:02:20 +0200

    day01: finished the exercise, used the youtube video for the explanation and paused to code whenever explanation were done

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,22 @@
+const keys = document.querySelectorAll(".key");
+window.addEventListener("keydown",playSound);
+
+keys.forEach((key) => 
+	key.addEventListener("transitionend",removeTransition) 
+	
+);
+
+function removeTransition(e){
+	if (e.propertyName != 'transform')return;
+	console.log(e.propertyName);
+	this.classList.remove("playing");
+}
+
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key=${e.code}]`)
+	const key = document.querySelector(`.key[data-key=${e.code}]`)
+	if (!audio) return;
+	audio.currentTime =0;
+	audio.play();
+	key.classList.add("playing");
+}
